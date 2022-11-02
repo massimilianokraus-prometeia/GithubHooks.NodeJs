abd1b36524338f28fd618ac3e3cbbb69bb5be933
massimilianokraus-prometeia <103130893+massimilianokraus-prometeia@users.noreply.github.com>
2022-11-02 12:22:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const args = process.argv.slice(3);
-console.log($"NodeJs \n- Commit Hash: {args[0]} \n - Author: {args[1]} \n - Message: {args[2]}")
+console.log("NodeJs")
+console.log("- Commit Hash:", args[0])
+console.log("- Author:", args[1])
+console.log("- Message:", args[2])
